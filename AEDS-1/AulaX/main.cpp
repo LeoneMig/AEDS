44a85e1b107f7907a7156172534196ae9f5cfbf0
Miguel Leone <miguel.silva@sou.unifal-mg.edu.br>
2024-08-07 10:12:32 -0300

    Add files via upload

---
+++ AEDS-1/AulaX/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 2024.1.08.019
+ *
+ * Created on 4 de março de 2024, 17:28
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
